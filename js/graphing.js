8bce24d04cb5c9ebe7347d48b22861e4548eaf60
Laura Herlant <herlant@cmu.edu>
2016-01-19 00:32:06 -0500

    auto scaling of fft

--- js/graphing.js
+++ js/graphing.js
@@ -1,5 +1,10 @@
 
 var drawVisual
+var min;
+var max;
+var i_min;
+var i_max;
+
 
 function visualize() {
     var canvas = document.querySelector('.visualizer');
@@ -14,19 +19,44 @@ function visualize() {
     var right_data = new Float32Array(right_buffer_length);
     
     canvasCtx.clearRect(0,0, WIDTH, HEIGHT);
+
+    var sample_rate = window.context.sampleRate;
     
     function draw() {
         drawVisual = requestAnimationFrame(draw);
         
+        // Get Data
         player.left_analyser_node.getFloatFrequencyData(left_data);
         player.right_analyser_node.getFloatFrequencyData(right_data);
         
+        // get start and end indices based on frequency range
+        var min_freq = Math.min(player.left_f0, player.right_f0) - 100;
+        var max_freq = Math.max(player.left_f0, player.right_f0)*player.n_harmonics + 100;
+        
+        i_min = Math.round(min_freq*player.left_analyser_node.fftSize / sample_rate);
+        i_max = Math.round(max_freq*player.left_analyser_node.fftSize / sample_rate);
+        
+        
+        // Get y min and max
         var left_min = Math.min.apply(Math,left_data);
         var left_max = Math.max.apply(Math,left_data);
         var right_min = Math.min.apply(Math,right_data);
         var right_max = Math.max.apply(Math,right_data);
-        var min = Math.min(left_min, right_min);
-        var max = Math.max(left_max, right_max);
+        
+        if(player.right_en && player.left_en) {
+            min = Math.min(left_min, right_min);
+            max = Math.max(left_max, right_max);
+        } else if(player.right_en && !player.left_en) {
+            min = right_min/2;
+            max = right_max;
+        } else if(!player.right_en && player.left_en) {
+            min = left_min/2;
+            max = left_max;
+        } else {
+            canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+            canvasCtx.fillRect(0,0, WIDTH, HEIGHT);
+            return;
+        }
         
         if(max != min) {
             scale = HEIGHT/(max-min);
@@ -34,6 +64,8 @@ function visualize() {
             scale = 1;
         }
         
+        
+
         canvasCtx.fillStyle = 'rgb(0, 0, 0)';
         canvasCtx.fillRect(0,0, WIDTH, HEIGHT);
         canvasCtx.lineWidth = 2;
@@ -42,9 +74,9 @@ function visualize() {
         
         var x=0;
         var y=0;
-        var delx = WIDTH / left_buffer_length;
+        var delx = WIDTH / (i_max-i_min);
         
-        for(var i=0; i<left_buffer_length; i++) {
+        for(var i=i_min; i<i_max; i++) {
             var v = (left_data[i]-min)*scale;
             var y = HEIGHT - v;
             if(i==0) {
@@ -60,7 +92,7 @@ function visualize() {
         canvasCtx.strokeStyle = 'rgb(255, 0, 0)';
         canvasCtx.beginPath();
         x=0;
-        for(var i=0; i<right_buffer_length; i++) {
+        for(var i=i_min; i<i_max; i++) {
             var v = (right_data[i]-min)*scale;
             var y = HEIGHT - v;
             if(i==0) {
