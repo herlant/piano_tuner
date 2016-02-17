cdf913c91d6a56aba237bdce5ea229f8fdf532af
Laura Herlant <herlant@cmu.edu>
2016-02-16 20:18:04 -0500

    Implemented load note

--- js/herlant.js
+++ js/herlant.js
@@ -129,9 +129,11 @@ function change_frequency(channel, val)
     switch(channel) {
         case "left":
             player.left_f0 = val;
+			document.getElementById('left_f0').value = val;
         break;
         case "right":
             player.right_f0 = val;
+			document.getElementById('right_f0').value = val;
         break;
     }
     player.update();
@@ -217,93 +219,12 @@ function enable_mic(v)
     set_fft_size(document.getElementById("fft_size").value);
 }
 
-var noteNames = [ "", 
-    "A0 Double Pedal A",
-    "A♯0/B♭0",
-    "B0",
-    "C1 Pedal C",
-    "C♯1/D♭1",
-    "D1",
-    "D♯1/E♭1",
-    "E1",
-    "F1",
-    "F♯1/G♭1",
-    "G1",
-    "G♯1/A♭1",
-    "A1",
-    "A♯1/B♭1",
-    "B1",
-    "C2 Deep C",
-    "C♯2/D♭2",
-    "D2",
-    "D♯2/E♭2",
-    "E2",
-    "F2",
-    "F♯2/G♭2",
-    "G2",
-    "G♯2/A♭2",
-    "A2",
-    "A♯2/B♭2",
-    "B2",
-    "C3 Tenor C",
-    "C♯3/D♭3",
-    "D3",
-    "D♯3/E♭3",
-    "E3",
-    "F3",
-    "F♯3/G♭3",
-    "G3",
-    "G♯3/A♭3",
-    "A3",
-    "A♯3/B♭3",
-    "B3",
-    "C4 Middle C",
-    "C♯4/D♭4",
-    "D4",
-    "D♯4/E♭4",
-    "E4",
-    "F4",
-    "F♯4/G♭4",
-    "G4",
-    "G♯4/A♭4",
-    "A4",
-    "A♯4/B♭4",
-    "B4",
-    "C5",
-    "C♯5/D♭5",
-    "D5",
-    "D♯5/E♭5",
-    "E5",
-    "F5",
-    "F♯5/G♭5",
-    "G5",
-    "G♯5/A♭5",
-    "A5",
-    "A♯5/B♭5",
-    "B5",
-    "C6 Soprano C",
-    "C♯6/D♭6",
-    "D6",
-    "D♯6/E♭6",
-    "E6",
-    "F6",
-    "F♯6/G♭6",
-    "G6",
-    "G♯6/A♭6",
-    "A6",
-    "A♯6/B♭6",
-    "B6",
-    "C7 Double high C",
-    "C♯7/D♭7",
-    "D7",
-    "D♯7/E♭7",
-    "E7",
-    "F7",
-    "F♯7/G♭7",
-    "G7",
-    "G♯7/A♭7",
-    "A7",
-    "A♯7/B♭7",
-    "B7",
-    "C8"
-];    
+function load_note(channel)
+{
+	var note = document.getElementById('note').value;
+	var oct = document.getElementById('octave').value;
+	var ref = document.getElementById('ref').value;
+	var freq = ref*Math.pow(2, (note-9)/12)*Math.pow(2,oct-4);
+	freq = Math.round(freq*100)/100;
+	change_frequency(channel,freq);
+}
